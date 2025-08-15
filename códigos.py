52a3aa9edb8751f410db7a4c64b4542758009b8a
Bryan samuebr <00001139730046sp@al.educacao.sp.gov.br>
2025-08-15 00:10:15 +0000

    Atualização

--- códigos.py
+++ códigos.py
@@ -1,1 +1,10 @@
+idade = int (input("Digite sua idade?"))
 
+if idade >= 19:
+    print("Você e de maior")
+
+elif idade >= 16:
+    print("Você e de menor, mais com acesso ao um responsável")
+    
+else:
+    print("Você e de menor")
